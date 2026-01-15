69386721a119c676f3abb79943834d64fee200b7
Shavirafn <73510961+shafenad@users.noreply.github.com>
2026-01-15 11:13:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
-});
+
+    optimizeDeps: {
+        exclude: ['jquery'], 
+    },
+
+    resolve: {
+        alias: {
+            $: 'jquery',
+            jQuery: 'jquery',
+        },
+    },
+})
